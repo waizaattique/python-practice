191b60884bb4414b64b0a8b5084f9eb21165b18b
waizaattique <waizaaattiquekhan@gmail.com>
2026-09-17 00:28:11 +0500

    add python practice

---
+++ basics/operators.py
@@ -0,0 +1,64 @@
+#Addition
+
+#Question no 1 
+
+a = 15
+b = 20
+sum = a + b
+print(sum)       #Output : 35
+
+#Question no 2
+
+a,b,c = 10, 5, 3
+print(a + b + c)  #Output : 18
+
+#Question no 3
+
+a , b = 10 , 5
+print(a + b * 2)  #Output : 20
+
+#Question no 4 
+
+a , b = 20,5 
+print((a+b)*2)  #Output : 50 
+
+#Question no 5 
+
+x, y, z = 4, 6, 2
+print(x + y * z + 1)  #output : 17 
+
+#Question no 6 
+
+A, B, C = 15, 4, 3
+print(A + B * C)  #Output : 27
+print((A + B) * C)  #Output : 57
+
+# Subtractions 
+
+#Question no 1 
+
+a = 100
+b =200
+c = 365
+diff = c-(b-a)
+print (diff)   #Output : 265 
+
+#Question no 2 
+
+a, b , c = 30 , 8 , 4 
+print  (a - b - c)  #Outout : 18 
+
+#Question no 3
+
+a , b = 20 , 5
+print ( a- b *2)  #Output : 10
+
+#Question no 4 
+
+a, b = 20 , 5 
+print((a-b)* 2)
+
+#Question no 5
+
+x, y, z = 50, 12, 8
+print(x - y + z)   #Output : 46
--- sum.py
+++
@@ -1,6 +0,0 @@
-a = 15
-b = 20
-sum = a + b
-print("The sum of a and b is:", sum)
-#It can be also written as :
-print(sum)
